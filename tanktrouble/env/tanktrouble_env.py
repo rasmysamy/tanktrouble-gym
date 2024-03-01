b57fd2bd391164a2de7cfd79c36f6676320ea953
samyr <samyrasmy0@gmail.com>
2024-03-01 01:19:50 -0500

    Physics done

--- tanktrouble/env/tanktrouble_env.py
+++ tanktrouble/env/tanktrouble_env.py
@@ -14,7 +14,7 @@ from gymnasium.spaces import Discrete, MultiDiscrete, Box, Tuple, MultiBinary
 
 
 class Ball:
-    def __init__(self, x=-100, y=-100, vx=-100, vy=-100, life=-1):
+    def __init__(self, x=-100.0, y=-100.0, vx=-100.0, vy=-100.0, life=-1):
         self.x = x
         self.y = y
         self.vx = vx
@@ -33,7 +33,7 @@ class TankTrouble(ParallelEnv):
     def __init__(self, s_x=8, s_y=5):
         self.tank_length = 0.5
         self.tank_width = 0.3
-        self.tank_speed = 1.25
+        self.tank_speed = 0.05
         self.size_x = s_x
         self.size_y = s_y
         self.p1_x = None
@@ -42,18 +42,16 @@ class TankTrouble(ParallelEnv):
         self.p2_y = None
         self.p1_direction = None
         self.p2_direction = None
-        self.remaining_time = 100
+        self.remaining_time = 10000
         self.possible_agents = [0, 1]
-        self.ball_speed = 2
-        self.max_speed = 5
+        self.ball_speed = self.tank_speed*2
+        self.max_speed = 5.0
         self.p_wall = 0.4
 
         self.min_start_dist = max(self.size_x, self.size_y) / 1.33
 
-        self.p1_vx = None
-        self.p1_vy = None
-        self.p2_vx = None
-        self.p2_vy = None
+        self.p1_v = 0.0
+        self.p2_v = 0.0
 
         self.agent_name_mapping = {0: "p1", 1: "p2"}
         self.max_balls = 5
@@ -142,10 +140,8 @@ class TankTrouble(ParallelEnv):
 
         self.p1_direction = random.random() * 2 * 3.14159
         self.p2_direction = random.random() * 2 * 3.14159
-        self.p1_vx = 0
-        self.p1_vy = 0
-        self.p2_vx = 0
-        self.p2_vy = 0
+        self.p1_v = 0.0
+        self.p2_v = 0.0
         self.remaining_time = 100
         self.p1_balls = [Ball() for _ in range(self.max_balls)]
         self.p2_balls = [Ball() for _ in range(self.max_balls)]
@@ -153,9 +149,9 @@ class TankTrouble(ParallelEnv):
         while True:
             self.horizontal_walls = [[random.random() - (1 if idx in [0, self.size_y] else 0) < self.p_wall for idx in
                                       range(self.size_y + 1)] for _ in
-                                     range(self.size_x)]  # outside walls are always there
+                                     range(self.size_x+1)]  # outside walls are always there
             self.vertical_walls = [
-                [random.random() - (1 if idx in [0, self.size_x] else 0) < self.p_wall for _ in range(self.size_y)] for
+                [random.random() - (1 if idx in [0, self.size_x] else 0) < self.p_wall for _ in range(self.size_y+1)] for
                 idx in range(self.size_x + 1)]  # outside walls are always there
             # check if there is a path from p1 to p2
             if self.is_path(self.p1_x, self.p1_y, self.p2_x, self.p2_y):
@@ -201,8 +197,27 @@ class TankTrouble(ParallelEnv):
         # draw a rectangle at x, y with the direction
         rect = plt.Rectangle((x - self.tank_length / 2, y - self.tank_width / 2), self.tank_length, self.tank_width,
                              angle=direction * 180 / 3.14159, color=color, rotation_point=(x, y))
-        plt.gca().add_patch(rect)
-        plt.plot([x, x + 0.5 * math.cos(direction)], [y, y + 0.5 * math.sin(direction)], color)
+
+        px = x
+        py = y
+        rot = direction
+
+        c1x = px + (self.tank_length / 2) * math.cos(rot) + (self.tank_width / 2) * math.cos(rot + math.pi / 2)
+        c1y = py + (self.tank_length / 2) * math.sin(rot) + (self.tank_width / 2) * math.sin(rot + math.pi / 2)
+        c2x = px + (self.tank_length / 2) * math.cos(rot) - (self.tank_width / 2) * math.cos(rot + math.pi / 2)
+        c2y = py + (self.tank_length / 2) * math.sin(rot) - (self.tank_width / 2) * math.sin(rot + math.pi / 2)
+        c3x = px - (self.tank_length / 2) * math.cos(rot) + (self.tank_width / 2) * math.cos(rot + math.pi / 2)
+        c3y = py - (self.tank_length / 2) * math.sin(rot) + (self.tank_width / 2) * math.sin(rot + math.pi / 2)
+        c4x = px - (self.tank_length / 2) * math.cos(rot) - (self.tank_width / 2) * math.cos(rot + math.pi / 2)
+        c4y = py - (self.tank_length / 2) * math.sin(rot) - (self.tank_width / 2) * math.sin(rot + math.pi / 2)
+
+        plt.plot(c1x, c1y, 'ro')
+        plt.plot(c2x, c2y, 'ro')
+        plt.plot(c3x, c3y, 'ro')
+        plt.plot(c4x, c4y, 'ro')
+
+        # plt.gca().add_patch(rect)
+        # plt.plot([x, x + 0.5 * math.cos(direction)], [y, y + 0.5 * math.sin(direction)], color)
 
     def project(self, v1, v2):
         # project v1 onto v2
@@ -226,8 +241,7 @@ class TankTrouble(ParallelEnv):
         dy = p2[1] - p1[1]
         return self.normalize([-dy, dx])
 
-    def player_segment_collision(self, px, py, rot, x1, y1, x2, y2):
-        # first, we find the 4 corners of the tank
+    def get_player_corners(self, px, py, rot):
         c1x = px + (self.tank_length / 2) * math.cos(rot) + (self.tank_width / 2) * math.cos(rot + math.pi / 2)
         c1y = py + (self.tank_length / 2) * math.sin(rot) + (self.tank_width / 2) * math.sin(rot + math.pi / 2)
         c2x = px + (self.tank_length / 2) * math.cos(rot) - (self.tank_width / 2) * math.cos(rot + math.pi / 2)
@@ -242,35 +256,254 @@ class TankTrouble(ParallelEnv):
         c3 = [c3x, c3y]
         c4 = [c4x, c4y]
 
+        return c1, c2, c3, c4
+    def player_segment_collision(self, px, py, rot, x1, y1, x2, y2):
+        # first, we find the 4 corners of the tank
+        c1, c2, c3, c4 = self.get_player_corners(px, py, rot)
+
+
+
         # we can use the separating axis theorem to check if the player segment collides with the wall segment
         # lets first project onto the normal of the wall segment
-        axis = self.perp_axis_from_points([x1, y1], [x2, y2])
 
-        if not self.sat_test([c1, c2, c3, c4], [[x1, y1], [x2, y2]], axis):
+        if not self.sat_test([[x1, y1], [x2, y2]], [c1, c2, c3, c4], [1, 0]):
+            return False
+
+        if not self.sat_test([[x1, y1], [x2, y2]], [c1, c2, c3, c4], [0, 1]):
             return False
 
         # now we project onto the normal of the player segment
-        axis = self.perp_axis_from_points([c1x, c1y], [c2x, c2y])
+        axis = [math.cos(rot + math.pi/2), math.sin(rot + math.pi/2)]
         if not self.sat_test([[x1, y1], [x2, y2]], [c1, c2, c3, c4], axis):
             return False
 
         # now we project onto the player segment
         axis = [math.cos(rot), math.sin(rot)]
-        if not self.sat_test([[c1x, c1y], [c2x, c2y], [c3x, c3y], [c4x, c4y]], [[x1, y1], [x2, y2]], axis):
+        if not self.sat_test([c1, c2, c3, c4], [[x1, y1], [x2, y2]], axis):
             return False
 
+        return True
+
+    def player_collision(self, px, py, rot):
+        # we check if the player collides with any wall
+        for i in range(self.size_x+1):
+            for j in range(self.size_y+1):
+                if self.horizontal_walls[i][j]:
+                    if self.player_segment_collision(px, py, rot, i, j, i + 1, j):
+                        print("collision", i, j, "vertical")
+                        return True
+                if self.vertical_walls[i][j]:
+                    if self.player_segment_collision(px, py, rot, i, j, i, j + 1):
+                        print("collision", i, j, "vertical")
+                        perp = self.perp_axis_from_points([i, j], [i, j + 1])
+                        projected_player = [self.project([px, py], perp)]
+                        return True
+        return False
+
+    def segment_collision(self, s1, s2):
+        s1p1 = [s1[0], s1[1]]
+        s1p2 = [s1[2], s1[3]]
+        s2p1 = [s2[0], s2[1]]
+        s2p2 = [s2[2], s2[3]]
+        ax1 = self.normalize([s1[2] - s1[0], s1[3] - s1[1]])
+        ax2 = self.normalize([s2[2] - s2[0], s2[3] - s2[1]])
+        ax3 = self.perp_axis_from_points(s1p1, s1p2)
+        ax4 = self.perp_axis_from_points(s2p1, s2p2)
+        if not self.sat_test([s1p1, s1p2], [s2p1, s2p2], ax1):
+            return False
+        if not self.sat_test([s1p1, s1p2], [s2p1, s2p2], ax2):
+            return False
+        if not self.sat_test([s1p1, s1p2], [s2p1, s2p2], ax3):
+            return False
+        if not self.sat_test([s1p1, s1p2], [s2p1, s2p2], ax4):
+            return False
+        return True
+
+
+
+    def ball_step(self, ball, t=0):
+        eps = 0.00001
+        if t >= 1:
+            return ball.x, ball.y, ball.vx, ball.vy
+        next_x = ball.x + ball.vx
+        next_y = ball.y + ball.vy
+        current_x = ball.x + ball.vx * t
+        current_y = ball.y + ball.vy * t
+        segment = [current_x, current_y, next_x, next_y]
+        collisions_h = []
+        collisions_v = []
+        for i in range(self.size_x+1):
+            for j in range(self.size_y+1):
+                if self.horizontal_walls[i][j]:
+                    if self.segment_collision(segment, [i, j, i + 1, j]):
+                        collision_time = (j - ball.y) / ball.vy
+                        collisions_h.append((collision_time, i, j))
+                if self.vertical_walls[i][j]:
+                    if self.segment_collision(segment, [i, j, i, j + 1]):
+                        collision_time = (i - ball.x) / ball.vx
+                        collisions_v.append((collision_time, i, j))
+        collisions_h = sorted(collisions_h, key=lambda x: x[0])
+        collisions_v = sorted(collisions_v, key=lambda x: x[0])
+        if len(collisions_h) == 0 and len(collisions_v) == 0:
+            return next_x, next_y, ball.vx, ball.vy
+        min_t_h = 10000000 if len(collisions_h) == 0 else collisions_h[0][0]
+        min_t_v = 10000000 if len(collisions_v) == 0 else collisions_v[0][0]
+        if min(min_t_h, min_t_v) + t > 1:
+            return next_x, next_y, ball.vx, ball.vy
+        if min_t_h < min_t_v:
+            # we step to the point of collision, and then we invert the y velocity, and we step again
+            collision_x = ball.x + ball.vx * min_t_h
+            collision_y = ball.y + ball.vy * min_t_h
+            new_vy = -ball.vy
+            new_vx = ball.vx
+            return self.ball_step(Ball(collision_x + eps*new_vx, collision_y+eps*new_vy, new_vx, new_vy, ball.life), t + min_t_h)
+        else:
+            collision_x = ball.x + ball.vx * min_t_v
+            collision_y = ball.y + ball.vy * min_t_v
+            new_vx = -ball.vx
+            new_vy = ball.vy
+            return self.ball_step(Ball(collision_x + eps*new_vx, collision_y+eps*new_vy, new_vx, new_vy, ball.life), t + min_t_v)
+
+    def player_player_collision(self, p1x, p1y, p1rot, p2x, p2y, p2rot):
+        c1, c2, c3, c4 = self.get_player_corners(p1x, p1y, p1rot)
+        c5, c6, c7, c8 = self.get_player_corners(p2x, p2y, p2rot)
+
+        if not self.sat_test([c1, c2, c3, c4], [c5, c6, c7, c8], [1, 0]):
+            return False
+        if not self.sat_test([c1, c2, c3, c4], [c5, c6, c7, c8], [0, 1]):
+            return False
+        if not self.sat_test([c1, c2, c3, c4], [c5, c6, c7, c8], [math.cos(p1rot), math.sin(p1rot)]):
+            return False
+        if not self.sat_test([c1, c2, c3, c4], [c5, c6, c7, c8], [math.cos(p2rot), math.sin(p2rot)]):
+            return False
+        if not self.sat_test([c1, c2, c3, c4], [c5, c6, c7, c8], [math.cos(p1rot + math.pi/2), math.sin(p1rot + math.pi/2)]):
+            return False
+        if not self.sat_test([c1, c2, c3, c4], [c5, c6, c7, c8], [math.cos(p2rot + math.pi/2), math.sin(p2rot + math.pi/2)]):
+            return False
         return True
 
 
 
 
-    def player_collision(self, px, py, rot):
-        # check if
-        pass
     def step(self, actions):
+        acc_time = 15
+
+        # we get the actions from the agents
+        p1_action = actions[0]
+        p2_action = actions[1]
+
+        p1_original_x = self.p1_x
+        p1_original_y = self.p1_y
+        p1_original_direction = self.p1_direction
+
+        p2_original_x = self.p2_x
+        p2_original_y = self.p2_y
+        p2_original_direction = self.p2_direction
+
+        # we apply the actions to the players
+        if p1_action[0]:
+            self.p1_v += self.tank_speed / acc_time
+        if p1_action[1]:
+            self.p1_v -= self.tank_speed / acc_time
+        if p1_action[2]:
+            self.p1_direction += 0.1
+        if p1_action[3]:
+            self.p1_direction -= 0.1
+        if self.p1_v > 0:
+            self.p1_v -= self.tank_speed / (acc_time * 2)
+            self.p1_v = max(self.p1_v, 0)
+        if self.p1_v < 0:
+            self.p1_v += self.tank_speed / (acc_time * 2)
+            self.p1_v = min(self.p1_v, 0)
 
 
-        pass
+        self.p1_v = min(self.p1_v, self.tank_speed)
+        self.p1_v = max(self.p1_v, -self.tank_speed)
+        self.p1_direction = self.p1_direction % (2 * 3.14159)
+
+        if p2_action[0]:
+            self.p2_v += self.tank_speed / acc_time
+        if p2_action[1]:
+            self.p2_v -= self.tank_speed / acc_time
+        if p2_action[2]:
+            self.p2_direction += 0.1
+        if p2_action[3]:
+            self.p2_direction -= 0.1
+        if self.p2_v > 0:
+            self.p2_v -= self.tank_speed / (acc_time * 2)
+            self.p2_v = max(self.p2_v, 0)
+        if self.p2_v < 0:
+            self.p2_v += self.tank_speed / (acc_time * 2)
+            self.p2_v = min(self.p2_v, 0)
+
+        front = [self.p1_x + 0.5 * math.cos(self.p1_direction) * self.tank_length, self.p1_y + 0.5 * math.sin(self.p1_direction) * self.tank_length]
+
+        if p1_action[4]:
+            self.p1_balls[1:-1] = self.p1_balls[0:-2]
+            self.p1_balls[0] = Ball(front[0], front[1], self.ball_speed * math.cos(self.p1_direction), self.ball_speed * math.sin(self.p1_direction), 100)
+        if p2_action[4]:
+            self.p2_balls[1:-1] = self.p2_balls[0:-2]
+            self.p2_balls[0] = Ball(front[0], front[1], self.ball_speed * math.cos(self.p2_direction), self.ball_speed * math.sin(self.p2_direction), 100)
+
+        for ball in self.p1_balls:
+            if ball.life > 0:
+                ball.x, ball.y, ball.vx, ball.vy = self.ball_step(ball)
+                ball.life -= 1
+        for ball in self.p2_balls:
+            if ball.life > 0:
+                ball.x, ball.y, ball.vx, ball.vy = self.ball_step(ball)
+                ball.life -= 1
+
+        for ball in self.p1_balls:
+            if ball.life > 0:
+                next_x = ball.x + ball.vx
+                next_y = ball.y + ball.vy
+
+
+        self.p2_v = min(self.p2_v, self.tank_speed)
+        self.p2_v = max(self.p2_v, -self.tank_speed)
+        self.p2_direction = self.p2_direction % (2 * 3.14159)
+        # we move the players
+        self.p1_x += self.p1_v * math.cos(self.p1_direction)
+        self.p1_y += self.p1_v * math.sin(self.p1_direction)
+        self.p2_x += self.p2_v * math.cos(self.p2_direction)
+        self.p2_y += self.p2_v * math.sin(self.p2_direction)
+
+        # we check for collisions with the walls
+        if self.player_collision(self.p1_x, self.p1_y, self.p1_direction) or self.player_player_collision(self.p1_x, self.p1_y, self.p1_direction, p2_original_x, p2_original_y, p2_original_direction):
+            self.p1_x = p1_original_x
+            self.p1_y = p1_original_y
+            self.p1_direction = p1_original_direction
+            self.p1_v = 0
+
+        if self.player_collision(self.p2_x, self.p2_y, self.p2_direction) or self.player_player_collision(self.p2_x, self.p2_y, self.p2_direction, p1_original_x, p1_original_y, p1_original_direction):
+            self.p2_x = p2_original_x
+            self.p2_y = p2_original_y
+            self.p2_direction = p2_original_direction
+            self.p2_v = 0
+
+        if self.player_player_collision(self.p1_x, self.p1_y, self.p1_direction, self.p2_x, self.p2_y, self.p2_direction):
+            self.p1_x = p1_original_x
+            self.p1_y = p1_original_y
+            self.p1_direction = p1_original_direction
+            self.p1_v = 0
+            self.p2_x = p2_original_x
+            self.p2_y = p2_original_y
+            self.p2_direction = p2_original_direction
+            self.p2_v = 0
+
+        observations = [None, None]
+        rewards = [0, 0]
+        dones = [False, False]
+        truncations = [False, False]
+        infos = [None, None]
+
+        self.remaining_time -= 1
+
+        if self.remaining_time <= 0:
+            dones = [True, True]
+
+        return observations, rewards, dones, truncations, infos
 
     def render(self):
         pass
--- tanktrouble/test.py
+++ tanktrouble/test.py
@@ -1,14 +1,51 @@
 import env.tanktrouble_env as tanktrouble
+from pynput import keyboard
 import matplotlib.pyplot as plt
 
+pressed = {'w': False, 's': False, 'a': False, 'd': False, 'up': False, 'down': False, 'left': False, 'right': False, 'fire': False}
+
+plt.rcParams['keymap.save'].remove('s')
+
+def on_press(key):
+    if isinstance(key, keyboard.Key):
+        return
+    print(key.char, "pressed")
+    pressed[key.char] = True
+
+
+def on_release(key):
+    if isinstance(key, keyboard.Key):
+        if key is keyboard.Key.space:
+            pressed['fire'] = True
+        return
+    print(key.char, "released")
+    pressed[key.char] = False
+
+
+listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+listener.start()
+
+env = tanktrouble.TankTrouble()
+env.reset()
+
 while True:
-    env = tanktrouble.TankTrouble()
-    env.reset()
-    env.step(0)
     plt.ion()
     plt.show()
     env.display_state(show=False)
     plt.draw()
-    plt.pause(1.0/120.0)
+    plt.pause(1.0 / 120.0)
+
+    actions_1 = [pressed['w'], pressed['s'], pressed['a'], pressed['d'], pressed['fire']]
+    pressed['fire'] = False
+
+    # print(actions_1)
+
+    env.step({0: actions_1, 1: [False, False, False, False, False]})
+
+    # print(env.p1_x)
+    # print(env.p1_y)
+    # print(env.p1_direction)
 
 j = input()
